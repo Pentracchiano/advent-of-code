4ce28e9774a9a0464422e782226f7df66323a282
Davide Cafaro <dvdcafaro@gmail.com>
2022-12-06 14:39:20 +0100

    added my version of python collections.Counter

---
+++ Tools/Collections.cs
@@ -0,0 +1,46 @@
+using System.Text;
+
+class Counter<TKey> : Dictionary<TKey, int> where TKey: notnull
+{
+    public Counter(IEnumerable<TKey> collection)
+    {
+        foreach(var key in collection)
+        {
+            this[key]++;
+        }
+    }
+
+    public Counter()
+    {
+
+    }
+
+    public new int this[TKey key]
+    {
+        get
+        {
+            int count = 0;
+            TryGetValue(key, out count);
+            return count;
+        }
+        set
+        {
+            if (value <= 0)
+            {
+                Remove(key);
+                return;
+            }
+
+            base[key] = value;
+        }
+    }
+
+    public new string ToString()
+    {
+        StringBuilder builder = new("Counter(");
+        builder.AppendJoin(", ", this);
+        builder.Append(")");
+        return builder.ToString();
+    }
+
+}
