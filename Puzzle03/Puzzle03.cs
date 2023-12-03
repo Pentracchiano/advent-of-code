36a14b99f1accb1b0fc9648db6f377234ca096bc
Davide Cafaro <davidecafaro@microsoft.com>
2023-12-03 23:39:41 +0100

    add puzzle 3 part 2

--- Puzzle03/Puzzle03.cs
+++ Puzzle03/Puzzle03.cs
@@ -1,5 +1,6 @@
 namespace Advent2023;
 
+using System;
 using System.ComponentModel;
 
 [Description("Gear Ratios")]
@@ -18,6 +19,27 @@ class Puzzle03 : PuzzleSolution
     public void Setup(string input)
     {
         map = Iterators.GetLines(input).ToList();
+
+        // build a graph of nodes, one number can span multiple nodes but will have one id.
+        int currentUniqueId = 0;
+        foreach (var row in map)
+        {
+            graph.Add(new List<Node>());
+            for (int j = 0; j < row.Length; j++)
+            {
+                if (!char.IsDigit(row[j]))
+                {
+                    graph[^1].Add(new Node(null, null, row[j]));
+                    continue;
+                }
+
+                string numberString = string.Concat(row!.Skip(j).TakeWhile(char.IsDigit));
+                int number = int.Parse(numberString);
+                j += numberString.Length - 1;
+                graph[^1].AddRange(Enumerable.Repeat(new Node(number, currentUniqueId, null), numberString.Length));
+                currentUniqueId++;
+            }
+        }
     }
 
     private bool IsAdjacentToSymbol(Queue<(int, int)> number)
@@ -70,9 +92,49 @@ class Puzzle03 : PuzzleSolution
         return sum.ToString();
     }
 
-    [Description("")]
+    [Description("What is the sum of all of the gear ratios in your engine schematic?\r\n\r\n")]
     public string SolvePartTwo()
     {
-        return "0";
+        // go left to right. find a STAR symbol, look for 2 numbers as neighbors.
+        // if they are exactly two, then add the multiplication of the numbers to the sum.
+        // the numbers are different if they have different ids. use the node.numberId to check.
+        // multiply using the node.number, not the node.numberId.
+        int sum = 0;
+
+        for (int i = 0; i < graph.Count; i++)
+        {
+            for (int j = 0; j < graph[i].Count; j++)
+            {
+                if (graph[i][j].symbol != '*')
+                {
+                    continue;
+                }
+
+                List<int> neighbors = NumberNeighbors(i, j);
+                if (neighbors.Count == 2)
+                {
+                    sum += neighbors[0] * neighbors[1];
+                }
+            }
+        }
+        return sum.ToString();
+    }
+
+    private List<int> NumberNeighbors(int x, int y)
+    {
+        HashSet<Node> neighbors = new HashSet<Node>(EqualityComparer<Node>.Create((n1, n2) => n1?.numberId == n2?.numberId, n => n.numberId.GetHashCode()));
+        foreach ((int i, int j) in directions)
+        {
+            if (x + i < graph.Count && x + i >= 0 && y + j < graph[x + i].Count && y + j >= 0)
+            {
+                if (graph[x + i][y + j].number != null)
+                {
+                    // in this way we'll have only one node for each number, even if the number spans multiple nodes
+                    // this also allows for different numbers that are equal in value
+                    neighbors.Add(graph[x + i][y + j]);
+                }
+            }
+        }
+        return neighbors.Select(n => n.number!.Value).ToList();
     }
 }
